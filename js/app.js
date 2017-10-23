335df8408cf0ee33ab5ed93365af119206856b1a
MagdaMB <magda.m.badura@gmail.com>
2017-10-23 10:28:40 +0200

    slider i kropki

--- js/app.js
+++ js/app.js
@@ -62,6 +62,30 @@
 			window.scroll(0,0);
 		})
 
+
+/*zadanie 5*/
+
+		var items = document.querySelectorAll(".org");
+		var dots = document.querySelectorAll(".dot");
+
+		console.log(items);
+		console.log(dots);
+
+		dots[0].addEventListener('click', function() {
+			for(var i=0; i<=dots.length-1; i++) {
+				dots[i].classList.remove('active');
+				items[i].classList.remove('visible');
+			}
+
+			this.classList.add('active');
+			items[0].classList.add('visible');
+
+			this.classList.add('active');
+			items[1].classList.add('visible');
+
+			this.classList.add('active');
+			items[2].classList.add('visible');
+		})
     });
 
 
